87ef6c6e935f2595fb982014f07d5d5d2018564f
NacasNils <guslyrni@student.gu.se>
2025-06-01 21:17:36 +0200

    Trying out vim in VSCode. also learning C#

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
-Console.WriteLine("The current time is " + DateTime.Now);
+﻿using System;
+using System.Threading;
 
+class Program
+{
+    static void Main()
+    {
+        int uAmount = 0;
+        string uName = "user";
+        Console.WriteLine("(This is test code written by Nils Lyrevik to learn .NET and C#!)");
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Hello! welcome to terminal gambling!");
+        Thread.Sleep(1000);
+
+        Console.WriteLine("What is your name? ");
+
+        uName = Console.ReadLine();
+
+        Console.WriteLine("How many coins does " + uName + " want to start with?");
+        while (uAmount < 10 || uAmount > 1000)
+        {
+            Console.WriteLine("please enter a number between 10 and 1000");
+        string tempAmount = Console.ReadLine();
+        uAmount = int.Parse(tempAmount);
+        }
+        Console.WriteLine("ok! amount is: " + uAmount);
+
+        }
+    }
